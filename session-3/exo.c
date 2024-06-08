6dcb011eccc998304be62b8b49936a00529a6077
Enone Fabien <enonefabien@Enones-MacBook-Pro.local>
2024-06-08 11:59:57 +0300

    solution

--- session-3/exo.c
+++ session-3/exo.c
@@ -2,6 +2,18 @@
 #include "./rand.c"
 int main() {
   int randomNumber = randMinMax(0, 100);
-  printf(" Random number is : %d \n", randomNumber);
+  int userAnswere;
+
+  while (randomNumber != userAnswere) {
+    printf("Guess the secret number between 0 and 100 ! \n");
+    scanf("%d", &userAnswere);
+    if (randomNumber > userAnswere ) {
+      printf("The secret number is greater \n");
+    } else if (randomNumber < userAnswere) {
+      printf("The secret number is smaller \n");
+    }
+  }
+
+  printf("Congratulation, the secret number was indeed : %d \n", randomNumber);
   return 0;
 }
