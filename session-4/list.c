34ab5ccfb7570b3123af1dc664c759eeae5af5a0
Enone Fabien <enonefabien@Enones-MacBook-Pro.local>
2024-06-15 00:31:34 +0300

    session-4 ready

--- session-4/list.c
+++ session-4/list.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
-#define MAX_SIZE 100
+#define SECOND_LIST_LENGHT 4
 
-int len(int list[]) {
-  return sizeof(&list) / sizeof(int);
-}
 
-int main()
-{
-  int myList[] = {0, 5, 7};
-  printf("%d\n", len(myList));
-  for (int i = 0; i <= len(myList); i++) {
-    printf("%d\n", myList[i]);
+int main() {
+  int firstList[] = {1, 5, 7, 9, 5};
+  
+  int firstListLength = sizeof(firstList) / sizeof(firstList[0]);
+  printf("Length of firstList : %d\n\n", firstListLength);
+  printf("List 1 >>>>>\n\n");
+  
+  for (int i = 0; i < firstListLength; i++) {
+    printf("index %d : %d\n",i ,firstList[i]);
+  }
+  
+  int secontList[SECOND_LIST_LENGHT] = { 5, 5, 4, 4 };
+  printf("List 2 >>>>>\n\n");
+  
+  for (int i = 0; i < SECOND_LIST_LENGHT; i++) {
+    printf("index %d : %d\n", i, secontList[i]);
   }
 
+  secontList[0] = 10;
+  secontList[3] = 10;
+  printf("List 2 after updates >>>>>\n\n");
+  for (int i = 0; i < SECOND_LIST_LENGHT; i++) {
+    printf("index %d : %d\n", i, secontList[i]);
+  }
   return 0;
 }
-
